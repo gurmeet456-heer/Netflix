2624bfe23e97d2f028dbebffd0bf4aa57caebc6c
gurmeet456-heer <gurmeet20042004@gmail.com>
2025-02-05 01:13:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -156,8 +156,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://netflix-8ecf1959e5ff.herokuapp.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://netflix-8ecf1959e5ff.herokuapp.com/".format(FQDN, PORT)
+URL = "https://commercial-reine-forward24k-8af69b8f.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://commercial-reine-forward24k-8af69b8f.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -172,9 +172,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://netflix-8ecf1959e5ff.herokuapp.com/".format(FQDN)
+    URL = "https://commercial-reine-forward24k-8af69b8f.koyeb.app/".format(FQDN)
 else:
-    URL = "https://netflix-8ecf1959e5ff.herokuapp.com/".format(FQDN)
+    URL = "https://commercial-reine-forward24k-8af69b8f.koyeb.app/".format(FQDN)
 
 # add premium logs channel id
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1002085696400'))
